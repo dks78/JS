a4d82533331a6f1113adf6e9e93c5a43ae7287bc
Aurélien <you@example.com>
2023-12-26 15:13:39 +0100

    chal 9 JS

---
+++ chalJS/chal9.js
@@ -0,0 +1,10 @@
+const hiddenMessage = ["X", "X", "X", "X", "W", "X", "E",  "X", "X", "X", "X", "X", "L", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "L", "X", "X", "X", "X", "X", "X", "X", "X", "X", " ","X", "X", "X", "X", "X", "X", "X", "X", "D", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "O", "X", "X", "X", "X", "X", "X", "N", "X", "X", "X", "X", "E", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", ",", "X", " ", "X"]
+
+for (let i = 0 ; i < hiddenMessage.length ; i++){
+    let lettre = hiddenMessage[i]
+    if ( lettre  === 'X' && hiddenMessage.includes('X') ){
+        console.log('_');
+    } else {
+        console.log(lettre);
+    }
+}
